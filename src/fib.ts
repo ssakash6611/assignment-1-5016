03c6b879d1c626d03d63781930a6f174ad59ddb6
ssakash6611 <ssakash6611@gmail.com>
2025-08-26 13:53:27 -0400

    Fixes src/fib.ts#L11 errors: n type and return type

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,11 @@
 // util function that computes the fibonacci numbers
-export default function fibonacci(n) {
+export function fibonacci(n: number): number | undefined {
   if (n < 0) {
-    return -1;
-  } else if (n == 0) {
+    return undefined;
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
-
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
